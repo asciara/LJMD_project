d7c7686565a6315a94acda404db183a64a81b1e2
Miguel Castellano <miguelcastellanomerino@gmail.com>
2020-02-12 20:56:11 +0100

    working force unit test

---
+++ ljmd-c/unit_testing/force/testforce.c
@@ -0,0 +1,79 @@
+/*
+ * Test function for force function. 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "data.h"
+#include "prototypes.h"
+#include <math.h>
+
+int main(int argc, char **argv) {
+
+    int nprint, i;
+    char restfile[BLEN], /*trajfile[BLEN], ergfile[BLEN],*/ line[BLEN];
+    FILE *fp;//,*traj,*erg;
+    mdsys_t sys;
+
+    /* read input file */
+    if(get_a_line(stdin,line)) return 1;
+    sys.natoms=atoi(line);
+    if(get_a_line(stdin,line)) return 1;
+    sys.mass=atof(line);
+    if(get_a_line(stdin,line)) return 1;
+    sys.epsilon=atof(line);
+    if(get_a_line(stdin,line)) return 1;
+    sys.sigma=atof(line);
+    if(get_a_line(stdin,line)) return 1;
+    sys.rcut=atof(line);
+    if(get_a_line(stdin,line)) return 1;
+    sys.box=atof(line);
+    if(get_a_line(stdin,restfile)) return 1;
+    /*if(get_a_line(stdin,trajfile)) return 1;
+    if(get_a_line(stdin,ergfile)) return 1;
+    if(get_a_line(stdin,line)) return 1;
+    sys.nsteps=atoi(line);
+    if(get_a_line(stdin,line)) return 1;
+    sys.dt=atof(line);
+    if(get_a_line(stdin,line)) return 1;
+    nprint=atoi(line);*/
+
+    /* allocate memory */
+    sys.rx=(double *)malloc(sys.natoms*sizeof(double));
+    sys.ry=(double *)malloc(sys.natoms*sizeof(double));
+    sys.rz=(double *)malloc(sys.natoms*sizeof(double));
+    /*sys.vx=(double *)malloc(sys.natoms*sizeof(double));
+    sys.vy=(double *)malloc(sys.natoms*sizeof(double));
+    sys.vz=(double *)malloc(sys.natoms*sizeof(double));*/
+    sys.fx=(double *)malloc(sys.natoms*sizeof(double));
+    sys.fy=(double *)malloc(sys.natoms*sizeof(double));
+    sys.fz=(double *)malloc(sys.natoms*sizeof(double));
+
+    /* read restart */
+    fp=fopen(restfile,"r");
+    if(fp) {
+        for (i=0; i<sys.natoms; ++i) {
+            fscanf(fp,"%lf%lf%lf",sys.rx+i, sys.ry+i, sys.rz+i);
+        }
+        /*for (i=0; i<sys.natoms; ++i) {
+            fscanf(fp,"%lf%lf%lf",sys.vx+i, sys.vy+i, sys.vz+i);
+        }*/
+        fclose(fp);
+        azzero(sys.fx, sys.natoms);
+        azzero(sys.fy, sys.natoms);
+        azzero(sys.fz, sys.natoms);
+    } else {
+        perror("cannot read restart file");
+        return 3;
+    }
+
+    /* initialize forces and energies.*/
+    sys.nfi=0;
+    force(&sys);
+
+    for (int i=0;i<sys.natoms;i++){
+	printf("Force on atom %d: (%f,%f,%f)\n",i,sys.fx[i],sys.fy[i],sys.fz[i]);
+    }
+    printf("\n");
+}
+
