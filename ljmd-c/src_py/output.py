142792aaccfc036988050570a1f5cca12eca72e8
Antonio Sciarappa <anto.sciara@gmail.com>
2020-02-13 16:49:59 +0100

    Add output.py

---
+++ ljmd-c/src_py/output.py
@@ -0,0 +1,12 @@
+from data import * 
+
+def output(system, erg, traj):
+    
+    print("%8d %20.8f %20.8f %20.8f %20.8f" % (system.nfi, system.temp, system.ekin, system.epot, system.ekin + system.epot))
+    
+    erg.write("%8d %20.8f %20.8f %20.8f %20.8f" % (system.nfi, system.temp, system.ekin, system.epot, system.ekin + system.epot))
+    
+    traj.write("%d\n nfi=%d etot=%20.8f" % (system.natoms, system.nfi, system.ekin + system.epot))
+    
+    for i in range(system.natoms):
+        traj.write("Ar  %20.8f %20.8f %20.8f" % (system.rx[i], system.ry[i], system.rz[i]))
