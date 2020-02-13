11d51e8022814b9569354ab151bf66fd8b5c891d
CeliaLaurent <claurent@inogs.it>
2020-02-13 15:34:13 +0100

    Adding function get_a_line in utilities.py

--- ljmd-c/src_py/utilities.py
+++ ljmd-c/src_py/utilities.py
@@ -9,3 +9,16 @@ def pbc(x,boxby2):
         print " << ",x
     return x
 
+# helper function: read a line and then return
+# the first string with whitespace stripped off 
+def get_a_line(line,string):
+     stripped = line.strip()
+     if not stripped:
+        return -1
+     string= line.split()[0]
+     if(string==''):
+        print "problem reading input"
+        return -1
+     else:
+        return 0
+
