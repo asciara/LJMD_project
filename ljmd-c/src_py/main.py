e61a4352f9f44760ffa0665a0bfca4878013596a
CeliaLaurent <claurent@inogs.it>
2020-02-13 16:48:55 +0100

    creating a function init_from_stdin in main

--- ljmd-c/src_py/main.py
+++ ljmd-c/src_py/main.py
@@ -1,18 +1,36 @@
+import sys
 from utilities import *
 import data
 
-trial=data.mdsys_t()
-trial.init_from_stdin()
-print trial.epsilon
-print trial.natoms
-print trial.nfi 
-print trial.nsteps
-print trial.dt
-print trial.mass
-print trial.epsilon
-print trial.sigma
-print trial.box 
-print trial.rcut
-print trial.ekin
-print trial.epot
-print trial.temp
+def create_from_stdin():
+   S=data.mdsys_t()
+   for count,line in enumerate(sys.stdin):
+       (R,value)=get_a_line(line)
+       if(R==0):
+          if(  count== 0): S.natoms  = int(value)
+          elif(count== 1): S.mass    = float(value)
+          elif(count== 2): S.epsilon = float(value)
+          elif(count== 3): S.sigma   = float(value)
+          elif(count== 4): S.rcut    = float(value)
+          elif(count== 5): S.box     = float(value)
+          elif(count== 6): restfile  = value
+          elif(count== 7): trajfile  = value
+          elif(count== 8): ergfile   = value
+          elif(count== 9): S.nsteps  = int(value)
+          elif(count==10): S.dt      = float(value)
+          elif(count==11): nprint    = int(value)
+   return S,restfile,trajfile,ergfile,nprint
+system,restfile,trajfile,ergfile,nprint=create_from_stdin()
+print system.epsilon
+print system.natoms  
+print system.nfi     
+print system.nsteps  
+print system.dt      
+print system.mass    
+print system.epsilon 
+print system.sigma   
+print system.box     
+print system.rcut    
+print system.ekin    
+print system.epot    
+print system.temp    
