d71b797c2088e361f1be33267db6afa29ffa3d3f
CeliaLaurent <claurent@inogs.it>
2020-02-13 16:30:47 +0100

    Trial to set _fields_ initialization from class function init_from_stdin

--- ljmd-c/src_py/main.py
+++ ljmd-c/src_py/main.py
@@ -1,5 +1,18 @@
-import sys
 from utilities import *
-for line in sys.stdin:
-    (R,value)=get_a_line(line)
-    if(R==0):print value
+import data
+
+trial=data.mdsys_t()
+trial.init_from_stdin()
+print trial.epsilon
+print trial.natoms
+print trial.nfi 
+print trial.nsteps
+print trial.dt
+print trial.mass
+print trial.epsilon
+print trial.sigma
+print trial.box 
+print trial.rcut
+print trial.ekin
+print trial.epot
+print trial.temp
